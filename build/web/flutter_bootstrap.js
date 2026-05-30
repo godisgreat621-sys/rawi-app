9d78e26ad2d7652b9ea1883b142079053d71b060
godisgreat621-sys <god.is.great.621@gmail.com>
2026-05-31 02:41:42 +0300

    وصف التحديث

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "155085010" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2374940062" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
